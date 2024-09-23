46fb1c12c27dde25b1b85b14cbfff5bb1e721f85
Jacob-Wojcik <jacob.a.wojcik@gmail.com>
2024-09-23 13:36:29 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Old School RuneScape Home Page</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+
+    <header>
+        <h1>Jacob's Old School RuneScape Lookup</h1>
+    </header>
+
+    <main>
+        <aside>
+            <h2>Widgets</h2>
+            <ul>
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="item_lookup.php">Lookup Item Info</a></li>
+                <li><a href="player_lookup.php">Lookup Player Info</a></li>
+            </ul>
+        </aside>
+
+        <section>
+            <h2>Main Content</h2>
+            <p>This is my website where you will be able to lookup individual item information such as:</p>
+            
+            <dl class="list1">
+            <dt class="bold-term">Item Information</dt>
+                <dd>- GE Price</dd>
+                <dd>- etc</dd>
+            </dl>
+             
+
+            <dl class="list2">
+            <dt class="bold-term">Player Information</dt>
+                <dd>- Level</dd>
+                <dd>- etc</dd>
+            </dl>
+        </section>
+    </main>
+
+</body>
+</html>
